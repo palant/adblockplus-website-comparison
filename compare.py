59926971048c8ae4079724dfd4ce300ccbaaedfc
Wladimir Palant <trev@adblockplus.org>
2015-04-20 16:23:43 +0200

    Normalize copy entities as well

--- compare.py
+++ compare.py
@@ -58,6 +58,7 @@ def normalize_contents(data):
   data = re.sub(r'([^>])[\r\n]+([^<])', r'\1 \2', data)
   data = re.sub(r'&mdash;', u'\u2014'.encode('utf-8'), data)
   data = re.sub(r'&nbsp;', u'\u00A0'.encode('utf-8'), data)
+  data = re.sub(r'&copy;', u'\u00A9'.encode('utf-8'), data)
   return data.strip()
 
 def process_anwiki_contents(data):
